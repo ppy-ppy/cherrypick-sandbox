ae0bb14471fc006b29b093059890419e03b5d8aa
Unknown <1173180105@qq.com>
2017-12-08 13:57:23 +0800

    add kmeans experiment

--- spearmint/examples/kmeans/userrun.py
+++ spearmint/examples/kmeans/userrun.py
@@ -8,22 +8,22 @@ def find_and_update_run(vm, cluster_size):
     exp = Experiment.find(EXPERIMENT)
 
     runs = exp.find_runs(vm, int(cluster_size))
-    run_time = run_and_log(vm)
+    run_time = run_and_log(vm, cluster_size)
     runs[0].time = runs[0].time * GAMMA + run_time
     runs[0].num = 0
     exp.count -= 1
     return runs[0]
 
 
-def run_and_log(vm):
-    command = "./run.sh 0 " + EXPERIMENT + " " + vm + " 1"
+def run_and_log(vm, cluster_size):
+    command = "./run.sh 0 " + EXPERIMENT + " " + vm + " " + cluster_size
     print os.system(command)
 
-    dirname = EXPERIMENT + '-' + 'local_type' + '-' + '1' + "-results"  # 'local_type' will be replaced by vm in the future.
+    dirname = EXPERIMENT + '-' + vm + '-' + str(cluster_size) + "-results"  # 'local_type' will be replaced by vm in the future.
 
     while os.path.exists(dirname) is False:
         continue
-    filename = dirname + '/1/' + 'local_type' + '.time'  # 'local_type' will be replaced by vm in the future.
+    filename = dirname + '/1/' + vm + '.time'  # 'local_type' will be replaced by vm in the future.
     f = open(filename)
     line = f.readline()
     run_time = float(line.split(',')[1])
@@ -47,5 +47,4 @@ def run_and_log(vm):
 
 
 if __name__ == '__main__':
-    # print find_and_update_run('d1.nano', 2)
     print find_and_update_run(*sys.argv[1:3])
--- spearmint/spearmint/config.py
+++ spearmint/spearmint/config.py
@@ -1,7 +1,5 @@
 import math
 
-EXPERIMENT = 'local'
-
 NOISE=0.020
 EI=0.05
 INITIAL_START=2
