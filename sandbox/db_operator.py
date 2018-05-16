4e15f1512be2c0dab7aa7bb787ac601308f210b7
Unknown <1173180105@qq.com>
2018-05-16 19:21:08 +0800

    add flavor in DB

--- sandbox/db_operator.py
+++ sandbox/db_operator.py
@@ -1,4 +1,5 @@
 from spearmint.schema import *
+import experiment
 
 
 def insert_job_info(job_id, user_id, cpu_percentage, io_percentage):
@@ -24,3 +25,12 @@ def insert_best_configuration(job_name, user_id, vm_name, vm_count, data_size, c
     job_id = JobInfo.selectBy(name=job_name, user_id=user_id).getOne().id
     config_id = Configuration.selectBy(count=vm_count, vm=vm_id).getOne().id
     BestConfiguration(job=job_id, data_size=data_size, config=config_id, cost=cost)
+
+
+def add_flavor(vm_name, vcpu, ram, disk, cost):
+    vm = experiment.VirtualMachine(vm_name, vcpu, ram, disk)
+    vm.insert_vm(cost)
+
+
+if __name__ == '__main__':
+    add_flavor('m1.2xlarge', 16, 32, 320, 0)
